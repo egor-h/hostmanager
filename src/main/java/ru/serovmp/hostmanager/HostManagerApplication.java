10b124a5f536881c522b0162dcceb195c02cafc2
Yegor H <procezorjk5@gmail.com>
2020-10-11 18:16:58 +0500

    initial

---
+++ src/main/java/ru/serovmp/hostmanager/HostManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.serovmp.hostmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/serovmp/hostmanager/HostManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serovmp.hostmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
