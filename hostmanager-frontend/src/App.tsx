c53443698b3bc8a814d42b3a811fa7334a2e3ad5
Yegor H <procezorjk5@gmail.com>
2020-12-13 15:46:45 +0500

    Introduce redux, types, basic files

--- hostmanager-frontend/src/App.tsx
+++ hostmanager-frontend/src/App.tsx
@@ -5,19 +5,25 @@ import { GlobalStyle } from './styles/GlobalStyle'
 import Greetings from './components/Greetings'
 import MenuColumn from './components/MenuColumn'
 import MainView from './components/MainView'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
 const mainElement = document.createElement('div')
 mainElement.setAttribute('id', 'root')
 document.body.appendChild(mainElement)
 
+const reducers = combineReducers({})
+const store = createStore(reducers)
+
 const App = () => {
   return (
     <>
-      <Router>
-        <MainView></MainView>
-      </Router>
-      
+      <Provider store={store}>
+        <Router>
+          <MainView></MainView>
+        </Router>
+      </Provider>
     </>
   )
 }
--- hostmanager-frontend/src/components/MenuColumn.tsx
+++ hostmanager-frontend/src/components/MenuColumn.tsx
@@ -1,48 +1,62 @@
 import React from 'react'
-import { Container } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { Link } from 'react-router-dom';
+import { Container } from '@material-ui/core'
+import IconButton from '@material-ui/core/IconButton'
+import HomeIcon from '@material-ui/icons/Home'
+import ComputerIcon from '@material-ui/icons/Computer'
+import ExploreIcon from '@material-ui/icons/Explore'
+import NoteIcon from '@material-ui/icons/Note'
+import SettingsIcon from '@material-ui/icons/Settings'
+// import DeleteIcon from '@material-ui/icons/Delete';
+import { Link } from 'react-router-dom'
 
 export default class MenuColumn extends React.Component {
+  render () {
+    return (<Container>
+      <Link to="/">
+        <IconButton aria-label="delete">
+          <HomeIcon />
+        </IconButton>
+      </Link>
+      <Link to="/objects">
+        <IconButton aria-label="delete">
+          <ComputerIcon />
+        </IconButton>
+      </Link>
+      <Link to="/map">
+        <IconButton aria-label="delete">
+          <ExploreIcon />
+        </IconButton>
+      </Link>
+      <Link to="/notes">
+        <IconButton aria-label="delete">
+          <NoteIcon />
+        </IconButton>
+      </Link>
+      <Link to="/settings">
+        <IconButton aria-label="delete">
+          <SettingsIcon />
+        </IconButton>
+      </Link>
+    </Container>)
+  }
 
-    render() {
-        return (<Container>
-                    <Link to="/">
-                        <IconButton aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
-                    </Link>
-                    <Link to="/objects">
-                        <IconButton aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
-                    </Link>
-                    <Link to="/map">
-                        <IconButton aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
-                    </Link>
-                </Container>)
-    }
-
-    // render() {
-    //     return (<Grid>
-    //         <Grid xs={1} >
-    //             <IconButton aria-label="delete">
-    //                 <DeleteIcon />
-    //             </IconButton>
-    //         </Grid>
-    //         <Grid xs={1} >
-    //             <IconButton aria-label="delete">
-    //                 <DeleteIcon />
-    //             </IconButton>
-    //         </Grid>
-    //         <Grid xs={1} >
-    //             <IconButton aria-label="delete">
-    //                 <DeleteIcon />
-    //             </IconButton>
-    //         </Grid>
-    //     </Grid>)
-    // }
+  // render() {
+  //     return (<Grid>
+  //         <Grid xs={1} >
+  //             <IconButton aria-label="delete">
+  //                 <DeleteIcon />
+  //             </IconButton>
+  //         </Grid>
+  //         <Grid xs={1} >
+  //             <IconButton aria-label="delete">
+  //                 <DeleteIcon />
+  //             </IconButton>
+  //         </Grid>
+  //         <Grid xs={1} >
+  //             <IconButton aria-label="delete">
+  //                 <DeleteIcon />
+  //             </IconButton>
+  //         </Grid>
+  //     </Grid>)
+  // }
 }
