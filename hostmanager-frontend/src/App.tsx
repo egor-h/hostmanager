6b4bd0f3fccf25b54b28d29d2a2462e6f00c1a86
Yegor H <procezorjk5@gmail.com>
2020-12-16 21:25:44 +0500

    add thunk middleware

--- hostmanager-frontend/src/App.tsx
+++ hostmanager-frontend/src/App.tsx
@@ -1,28 +1,26 @@
-import { Color, Titlebar } from 'custom-electron-titlebar';
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { combineReducers, createStore } from 'redux';
-import MainView from './components/MainView';
-import * as reducersRaw from './state/reducers';
-
+import { Color, Titlebar } from 'custom-electron-titlebar'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import MainView from './components/MainView'
+import * as reducersRaw from './state/reducers'
 
 const mainElement = document.createElement('div')
 mainElement.setAttribute('id', 'root')
 document.body.appendChild(mainElement)
 
-const reducers = combineReducers(reducersRaw);
-const store = createStore(reducers);
-
-
+const reducers = combineReducers(reducersRaw)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 window.addEventListener('DOMContentLoaded', () => {
-    let titlebar = new Titlebar({
-        backgroundColor: Color.fromHex("#F5F5F5")
-    });
-    titlebar.updateTitle("HostsManager app is to manage hosts!");
-});
+  const titlebar = new Titlebar({
+    backgroundColor: Color.fromHex('#F5F5F5')
+  })
+  titlebar.updateTitle('HostsManager app is to manage hosts!')
+})
 
 const App = () => {
   return (
