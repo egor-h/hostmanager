1982b2d49d714e602faa5477b19dc8c57c872e10
Yegor H <procezorjk5@gmail.com>
2020-12-20 17:40:25 +0500

    remove menubar (commented out)

--- hostmanager-frontend/electron/main.ts
+++ hostmanager-frontend/electron/main.ts
@@ -5,18 +5,19 @@ import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electro
 
 let mainWindow: Electron.BrowserWindow | null
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
     // backgroundColor: '#191622',
-    titleBarStyle: "hidden",
-    frame: false,
+    // titleBarStyle: "hidden",
+    // frame: false
     webPreferences: {
       // preload: path.join(__dirname, "renderer/preload.ts"),
       nodeIntegration: true
     }
   })
+  // mainWindow.removeMenu();
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL('http://localhost:4000')
@@ -41,10 +42,10 @@ app.on('ready', createWindow)
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
       installExtension(REDUX_DEVTOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
     }
   })
 app.allowRendererProcessReuse = true
