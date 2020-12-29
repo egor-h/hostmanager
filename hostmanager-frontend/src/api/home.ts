a32b1a91beafcd234cb035c63f6f6d4d3c9b0afd
Yegor H <procezorjk5@gmail.com>
2020-12-29 07:31:26 +0500

    use localhost address on current early version

--- hostmanager-frontend/src/api/home.ts
+++ hostmanager-frontend/src/api/home.ts
@@ -1,13 +1,13 @@
 import { home } from '../state/actions';
 import { getRequest } from './common';
 
-const API_RECENTS = "http://192.168.0.9:8080/api/v1/recent/objects";
+const API_RECENTS = "http://localhost:8080/api/v1/welcome";
 
 export const fetchRecents = () => {
     return getRequest({
         url: API_RECENTS,
-        actionBeforeFetch: home.recentHosts,
-        actionOnSuccess: home.recentHostsSucceeded,
-        actionOnError: home.recentHostsFailed
+        actionBeforeFetch: home.homepage,
+        actionOnSuccess: home.homepageSucceeded,
+        actionOnError: home.homepageFailed
     });
 }
