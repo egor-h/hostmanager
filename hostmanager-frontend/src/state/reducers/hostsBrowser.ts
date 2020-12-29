905ac21d8b14ff7ecdee8a3cfaaba28c22b272c9
Yegor H <procezorjk5@gmail.com>
2020-12-29 07:33:56 +0500

    commit unsaved changes

--- hostmanager-frontend/src/state/reducers/hostsBrowser.ts
+++ hostmanager-frontend/src/state/reducers/hostsBrowser.ts
@@ -1,5 +1,5 @@
 import { Host } from "../../models/host";
-import { HOST_TREE, HOST_TREE_FAILED, HOST_TREE_SUCCEDED, HOST_TABLE, HOST_TABLE_FAILED, HOST_TABLE_SUCCEDED } from "../constants";
+import { HOST_TREE, HOST_TREE_FAILED, HOST_TREE_SUCCEDED } from "../constants";
 
 
 export type TreeState = {
