7595993b9e0763f2d0b07ed03eb0b810f81ba7dc
Yegor H <someone@somewhere>
2020-12-29 16:43:13 +0500

    add new entity interfaces

--- hostmanager-frontend/src/models/host.ts
+++ hostmanager-frontend/src/models/host.ts
@@ -3,7 +3,21 @@ export interface Host {
     name: string,
     address: string,
     dir: boolean,
+    tags: string[],
+    protocols: number[],
     chld: Host[]
+}
+
+export interface Protocol {
+    id: number,
+    name: string,
+    executionLine: string,
+    launchType: "JUST_RUN" | "VALIDATE_EXITCODE" | "VALIDATE_OUTPUT" | "PRINT_OUTPUT" | "INTERNAL"
+}
+
+export interface Tag {
+    id: number,
+    name: string
 }
 
 export interface RecentHost {
