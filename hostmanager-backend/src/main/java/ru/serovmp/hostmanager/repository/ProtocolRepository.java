9e9611f52184b26ef36e9066665ba5ebef97670c
Yegor H <someone@somewhere>
2020-12-29 16:37:04 +0500

    create repos

--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/repository/ProtocolRepository.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/repository/ProtocolRepository.java
@@ -1,7 +1,9 @@
 package ru.serovmp.hostmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.serovmp.hostmanager.entity.Protocol;
 
+@Repository
 public interface ProtocolRepository extends JpaRepository<Protocol, Long> {
 }
--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/repository/TagRepository.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/repository/TagRepository.java
@@ -1,2 +1,13 @@
-package ru.serovmp.hostmanager.repository;public class TagRepository {
+package ru.serovmp.hostmanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.serovmp.hostmanager.entity.Tag;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(@Param("name") String name);
 }
