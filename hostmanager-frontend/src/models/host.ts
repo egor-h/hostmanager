91ec33c591fa283c993f591360f0e8fe8bc96ae5
Yegor H <procezorjk5@gmail.com>
2021-01-04 19:56:31 +0500

    update models for backend

--- hostmanager-frontend/src/models/host.ts
+++ hostmanager-frontend/src/models/host.ts
@@ -2,16 +2,28 @@ export interface Host {
     id: number,
     name: string,
     address: string,
+    enabled: boolean,
+    dir: boolean,
+    tags: Tag[],
+    protocols: number[],
+    chld: Host[]
+}
+
+export interface HostFormData {
+    name: string,
+    address: string,
+    enabled: boolean,
     dir: boolean,
     tags: string[],
     protocols: number[],
-    chld: Host[]
 }
 
 export interface Protocol {
     id: number,
     name: string,
     executionLine: string,
+    validationRegex: string,
+    expectedExitCode: number,
     launchType: "JUST_RUN" | "VALIDATE_EXITCODE" | "VALIDATE_OUTPUT" | "PRINT_OUTPUT" | "INTERNAL"
 }
 
