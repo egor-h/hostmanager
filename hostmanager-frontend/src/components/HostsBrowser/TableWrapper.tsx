22ca45661fe7c5fca4a1c9b51b787cb87b45b1ba
Yegor H <procezorjk5@gmail.com>
2021-01-09 16:11:06 +0500

    refactor, add new handlers for hostTable, make switch to table after confirm

--- hostmanager-frontend/src/components/HostsBrowser/TableWrapper.tsx
+++ hostmanager-frontend/src/components/HostsBrowser/TableWrapper.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory, useParams, useRouteMatch } from 'react-router-dom';
+import { deleteObject } from '../../api/tree';
 import { Host } from '../../models/host';
-import HostForm from './HostForm';
-import HostTable from './HostTable';
-import { setSelected, setBrowserEdit } from '../../state/actions/local';
+import { setSelected } from '../../state/actions/local';
 import { findHostById } from '../../util/tree';
+import HostTable from './HostTable';
 
 type ParamTypes = {
     parentId: string;
@@ -15,8 +15,6 @@ type PropType = {
     wholeTree: Host;
     selectedDir: string;
 }
-
-
 
 export default (props: PropType): any => {
     let match = useRouteMatch();
@@ -28,16 +26,17 @@ export default (props: PropType): any => {
             dispatch(setSelected(row.id+''));
             history.push(`/objects/table/${row.id}`);
         } else {
-            history.push(`/objects/edit/${row.id}`);
+            history.push(`/objects/info/${row.id}`);
         }
         
     }
     let hosts = findHostById(props.wholeTree, +parentId);
 
-    console.log(hosts);
-
     return (
-    <HostTable  
+    <HostTable
+        tableTitle={"Hosts"}
+        parentId={+parentId}
         onRowClicked={onRowClicked}
+        onEntryDelete={(row) => {dispatch(deleteObject(row.id))}}
         data={(hosts && hosts.dir) ? hosts.chld : []} />);
 }
