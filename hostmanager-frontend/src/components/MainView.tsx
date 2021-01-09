b9c4c2736fd173ca183a9d16e86b1e8ea731d033
Yegor H <procezorjk5@gmail.com>
2021-01-09 15:40:34 +0500

    add new routes, add interval scheduler to remove protocol results

--- hostmanager-frontend/src/components/MainView.tsx
+++ hostmanager-frontend/src/components/MainView.tsx
@@ -2,14 +2,24 @@ import { Grid } from '@material-ui/core';
 import { Titlebar } from 'custom-electron-titlebar';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { LocalState } from '../state/reducers/localState';
 import HostsBrowser from './HostsBrowser';
 import MainPage from './MainPage';
 import MenuColumn from './MenuColumn';
+import NotePage from './NotePage';
 import ProtocolPage from './ProtocolPage';
 import TagPage from './TagPage';
+import SearchPage from './SearchPage';
+import { bindActionCreators } from 'redux';
+import { cleanOldResultsThunk } from '../util/launcher';
+import { connect } from 'react-redux';
+import { setIntervalId } from '../state/actions/local';
+import StatisticPage from './StatisticPage';
 
 type PropsType = {
-    titlebar: Titlebar;
+    local: LocalState;
+    doCleanupResults: () => void;
+    setIntervalId: (id: number) => void
 }
 
 
@@ -26,9 +36,20 @@ const sideNavStyle = {
     overflow: 'auto',
   }
 
-export default class MainView extends React.Component {
+class MainView extends React.Component<PropsType> {
     constructor(props: PropsType) {
         super(props);
+    }
+
+    componentDidMount() {
+        let intervalId = setInterval(() => {
+            this.props.doCleanupResults();
+        }, 60000);
+        this.props.setIntervalId(intervalId);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.props.local.intervalId);
     }
 
     render() {
@@ -42,13 +63,13 @@ export default class MainView extends React.Component {
                         <HostsBrowser></HostsBrowser>
                     </Route>
                     <Route path="/search">
-                        <div>Search</div>
+                        <SearchPage />
                     </Route>
                     <Route path="/map">
                         <div>Object graph</div>
                     </Route>
                     <Route path="/notes">
-                        <div>Notes</div>
+                        <NotePage />
                     </Route>
                     <Route path="/protocols">
                         <ProtocolPage />
@@ -57,7 +78,10 @@ export default class MainView extends React.Component {
                         <TagPage />
                     </Route>
                     <Route path="/settings">
-                        <div>Settings there</div>
+                        <p>settings</p>
+                    </Route>
+                    <Route path="/stats">
+                        <StatisticPage />
                     </Route>
                     <Route path="/">
                         <MainPage></MainPage>
@@ -67,3 +91,14 @@ export default class MainView extends React.Component {
         </div>);
     }
 }
+
+const mapStateToProps = (state: {local: LocalState}) => ({
+    local: state.local
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+    doCleanupResults: bindActionCreators(cleanOldResultsThunk, dispatch),
+    setIntervalId: bindActionCreators(setIntervalId, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainView);
