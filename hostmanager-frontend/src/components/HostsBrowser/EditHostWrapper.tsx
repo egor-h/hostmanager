38f9ed59794a57aef6d938860c6ca2b1a39c9336
Yegor H <procezorjk5@gmail.com>
2021-01-09 15:55:00 +0500

    refactor and support editing directories

--- hostmanager-frontend/src/components/HostsBrowser/EditHostWrapper.tsx
+++ hostmanager-frontend/src/components/HostsBrowser/EditHostWrapper.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
-import { Host } from '../../models/host';
+import { saveObject } from '../../api/tree';
+import { Host, HostFormData } from '../../models/host';
+import { setBreadcrumb } from '../../state/actions/local';
 import { TreeState } from '../../state/reducers/hostsBrowser';
+import { ProtocolState } from '../../state/reducers/protocols';
+import { TagState } from '../../state/reducers/tags';
 import { findExactHostById } from '../../util/tree';
+import HostDirForm from './HostDirForm';
 import HostForm from './HostForm';
 
 type ParamTypes = {
@@ -13,24 +18,51 @@ type ParamTypes = {
 export default (): any => {;
     let { hostId } = useParams<ParamTypes>();
     let dispatch = useDispatch();
-
+    let history = useHistory();
     let tree = useSelector((state: {hostsBrowser: {tree: TreeState}}) => state.hostsBrowser.tree)
+    let tags = useSelector((state: {tags: TagState}) => state.tags);
+    let protocols = useSelector((state: {protocols: ProtocolState}) => state.protocols);
 
     if (tree.loading || tree.error) {
         return (<p>Error somehow</p>)
     }
 
     let foundHost = findExactHostById(tree.tree, +hostId);
-    
+
     if (! foundHost) {
         console.log(foundHost);
         return (<p>Host with {hostId} id not found</p>)
     }
 
-    return <HostForm 
+    return foundHost.dir ? <HostDirForm 
+        tags={tags.data}
         host={foundHost}
         onSubmit={(host: Host) => {
-            console.log(host);
+            dispatch(saveObject(host.id, {
+                name: host.name, 
+                address: host.address, 
+                enabled: host.enabled, 
+                dir: host.dir, 
+                tags: host.tags.map(t => t.name),
+                protocols: host.protocols
+            }));
+            history.goBack();
         }}
-        title={"EditHost"} />;
+        title={`Edit dir ${foundHost.name}`}/> : 
+    <HostForm 
+        tags={tags.data}
+        protocols={protocols.data}
+        host={foundHost}
+        onSubmit={(host: Host) => {
+            dispatch(saveObject(host.id, {
+                name: host.name, 
+                address: host.address, 
+                enabled: host.enabled, 
+                dir: host.dir, 
+                tags: host.tags.map(t => t.name),
+                protocols: host.protocols
+            }));
+            history.goBack();
+        }}
+        title={`Edit host ${foundHost.name}`} />;
 }
