c1675cd9d84aad895fdb618ee910a8d5c80ac357
Yegor H <procezorjk5@gmail.com>
2021-05-25 07:02:37 +0500

    refactor

--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/service/SearchService.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/service/SearchService.java
@@ -11,6 +11,7 @@ import ru.serovmp.hostmanager.repository.NoteRepository;
 import ru.serovmp.hostmanager.repository.ProtocolRepository;
 import ru.serovmp.hostmanager.repository.TagRepository;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,11 +32,24 @@ public class SearchService {
     }
 
     public BriefSearchResultDto briefSearch(String query) {
+        List<BriefSearchResultDto.BriefHost> hosts = hostRepository.topRecents(query, SEARCH_LIMIT).stream()
+                .map(h -> new BriefSearchResultDto.BriefHost(h.getId(), h.getName(), h.getAddress()))
+                .collect(Collectors.toList());
+        List<BriefNoteDto> notes = noteRepository.topRecents(query, SEARCH_LIMIT).stream()
+                .map(note -> new BriefNoteDto(note.getId(), note.getTitle(), note.getCreatedAt(), note.isDone()))
+                .collect(Collectors.toList());
+        List<ProtocolDto> protocols = protocolRepository.topRecents(query, SEARCH_LIMIT).stream()
+                .map(proto -> new ProtocolDto(proto.getId(), proto.getName(), proto.getExecutionLine(), proto.getLaunchType().name(), proto.getValidationRegex(), proto.getExpectedExitCode()))
+                .collect(Collectors.toList());
+        List<TagDto> tags = tagRepository.topRecents(query, SEARCH_LIMIT).stream()
+                .map(tag -> new TagDto(tag.getId(), tag.getName()))
+                .collect(Collectors.toList());
+
         return BriefSearchResultDto.builder()
-                .hosts(hostRepository.topRecents(query, SEARCH_LIMIT).stream().map(h -> new BriefSearchResultDto.BriefHost(h.getId(), h.getName(), h.getAddress())).collect(Collectors.toList()))
-                .notes(noteRepository.topRecents(query, SEARCH_LIMIT).stream().map(note -> new BriefNoteDto(note.getId(), note.getTitle(), note.getCreatedAt(), note.isDone())).collect(Collectors.toList()))
-                .protocols(protocolRepository.topRecents(query, SEARCH_LIMIT).stream().map(proto -> new ProtocolDto(proto.getId(), proto.getName(), proto.getExecutionLine(), proto.getLaunchType().name(), proto.getValidationRegex(), proto.getExpectedExitCode())).collect(Collectors.toList()))
-                .tags(tagRepository.topRecents(query, SEARCH_LIMIT).stream().map(tag -> new TagDto(tag.getId(), tag.getName())).collect(Collectors.toList()))
+                .hosts(hosts)
+                .notes(notes)
+                .protocols(protocols)
+                .tags(tags)
                 .build();
     }
 }
