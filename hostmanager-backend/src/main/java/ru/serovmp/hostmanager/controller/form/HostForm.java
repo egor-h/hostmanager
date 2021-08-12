3ddba90e4966af893d0d44bea46d9033cb997a6a
Yegor H <procezorjk5@gmail.com>
2021-08-12 11:46:36 +0500

    Add port constraint

--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/controller/form/HostForm.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/controller/form/HostForm.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.serovmp.hostmanager.validation.HostAddressConstraint;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @Data
@@ -16,6 +18,7 @@ public class HostForm {
     private String name;
     @HostAddressConstraint
     private String address;
+    @Min(0) @Max(65536)
     private int port;
     private boolean enabled;
     private boolean dir;
