3b166e077018ea8d02eecf2c83499e15aad687bc
Yegor H <procezorjk5@gmail.com>
2021-08-12 11:17:58 +0500

    global excetion handler && validation error handler

---
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/controller/GlobalExceptionHandlerController.java
@@ -0,0 +1,40 @@
+package ru.serovmp.hostmanager.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@ControllerAdvice
+@RestControllerAdvice
+public class GlobalExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity handleAll(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> fieldsToErrors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        Map<String, Object> response = Map.of(
+                "timestamp", new Date(),
+                "status", status.value(),
+                "errors", fieldsToErrors
+        );
+        return new ResponseEntity<>(response, headers, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}
