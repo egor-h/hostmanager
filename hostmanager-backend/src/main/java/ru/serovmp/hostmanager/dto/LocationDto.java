9895c79576fb5f03e1e42ef43b69f253b691e18c
Yegor H <procezorjk5@gmail.com>
2021-08-17 18:33:01 +0500

    add fields to location dto

--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/dto/LocationDto.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/dto/LocationDto.java
@@ -1,5 +1,16 @@
 package ru.serovmp.hostmanager.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.serovmp.hostmanager.entity.Location;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class LocationDto {
+    private long id;
+    private String name;
+    private String description;
 }
