7b4c4887ab0070465a5cffe26c526909a1522701
Yegor H <procezorjk5@gmail.com>
2021-09-04 11:32:51 +0500

    update database schema

--- hostmanager-backend/schema.sql
+++ hostmanager-backend/schema.sql
@@ -1,75 +1,136 @@
-CREATE TABLE `hosts` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `address` varchar(64) DEFAULT NULL,
-  `mac_address` varchar(12) DEFAULT NULL,
-  `dir` tinyint(1) DEFAULT NULL,
-  `port` int(11) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  `supported_protocols` varchar(128) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `who_created` varchar(64) DEFAULT NULL,
-  `enabled` tinyint(1) DEFAULT NULL,
-  `location` varchar(128) DEFAULT NULL,
-  `descr` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=962 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tags` (
+
+CREATE TABLE `locations` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `hosts_tags` (
-  host_id bigint(20) NOT NULL,
-  tags_id bigint(20) NOT NULL,
-  PRIMARY KEY (`host_id`, `tags_id`),
-  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`),
-  CONSTRAINT `fk_host_id` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
-);
+CREATE TABLE `notes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `done` bit(1) NOT NULL,
+  `done_at` datetime(6) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `protocols` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `execution_line` varchar(255) DEFAULT NULL,
+  `expected_exit_code` bigint(20) NOT NULL,
   `launch_type` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `validation_regex` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `roles` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `subnets` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `mask` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tags` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_login` (`login`),
+  KEY `idx_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `settings` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `key_name` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_user` (`user_id`),
+  CONSTRAINT `fk_setting_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `hosts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `enabled` bit(1) DEFAULT NULL,
+  `dir` bit(1) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `port` int(11) NOT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_created_by` (`created_by`),
+  KEY `idx_parent_id` (`parent_id`),
+  CONSTRAINT `fk_host_user` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_host_host` FOREIGN KEY (`parent_id`) REFERENCES `hosts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `hosts_locations` (
+  `host_id` bigint(20) NOT NULL,
+  `location_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`host_id`,`location_id`),
+  KEY `idx_hosts_location` (`location_id`),
+  CONSTRAINT `fk_location_host` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`),
+  CONSTRAINT `fk_host_location` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `hosts_notes` (
+  `host_id` bigint(20) NOT NULL,
+  `note_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`host_id`,`note_id`),
+  KEY `idx_note_id` (`note_id`),
+  CONSTRAINT `fk_note_host` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`),
+  CONSTRAINT `fk_host_note` FOREIGN KEY (`note_id`) REFERENCES `notes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `hosts_protocols` (
   `host_id` bigint(20) NOT NULL,
   `protocols_id` bigint(20) NOT NULL,
   PRIMARY KEY (`host_id`,`protocols_id`),
-  CONSTRAINT `fk_protocols_id` FOREIGN KEY (`protocols_id`) REFERENCES `protocols` (`id`),
-  CONSTRAINT `fk_host_id2` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
-);
+  KEY `idx_protocols_id` (`protocols_id`),
+  CONSTRAINT `fk_host_protocol` FOREIGN KEY (`protocols_id`) REFERENCES `protocols` (`id`),
+  CONSTRAINT `fk_protocol_host` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
-  `login` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `notes` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `title` varchar(255) DEFAULT '',
-    `text` TEXT DEFAULT '',
-    `done` tinyint(1) DEFAULT NULL,
-    `created_at` datetime DEFAULT NULL,
-    `created_by` bigint (20),
-    `done_at` datetime DEFAULT NULL
-)
-
-CREATE TABLE `hosts_notes` (
+CREATE TABLE `hosts_tags` (
   `host_id` bigint(20) NOT NULL,
-  `notes_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`host_id`,`notes_id`),
-  CONSTRAINT `fk_notes_id` FOREIGN KEY (`notes_id`) REFERENCES `notes` (`id`),
-  CONSTRAINT `fk_host_id3` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
-);
+  `tags_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`host_id`,`tags_id`),
+  KEY `idx_tag_id` (`tags_id`),
+  CONSTRAINT `fk_host_tag` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`),
+  CONSTRAINT `fk_tag_host` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `locations` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-)
+CREATE TABLE `users_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `idx_role_id` (`role_id`),
+  CONSTRAINT `fk_role_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
