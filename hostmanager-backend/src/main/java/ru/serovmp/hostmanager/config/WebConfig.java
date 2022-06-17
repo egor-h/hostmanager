2760e9b7d95a07e5e46fad0e23a0c5ca86600865
Yegor H <procezorjk5@gmail.com>
2022-06-17 22:03:49 +0500

    add origin for /api/v2

--- hostmanager-backend/src/main/java/ru/serovmp/hostmanager/config/WebConfig.java
+++ hostmanager-backend/src/main/java/ru/serovmp/hostmanager/config/WebConfig.java
@@ -10,6 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v2/*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+
         registry.addMapping("/api/v1/welcome/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
