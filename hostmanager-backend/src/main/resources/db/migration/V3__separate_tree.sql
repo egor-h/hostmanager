3061f236bfa5e327749592822a61e22ff0e8a742
Yegor H <procezorjk5@gmail.com>
2023-11-12 16:21:08 +0500

    fix sql migration script

--- hostmanager-backend/src/main/resources/db/migration/V3__separate_tree.sql
+++ hostmanager-backend/src/main/resources/db/migration/V3__separate_tree.sql
@@ -6,7 +6,7 @@ CREATE TABLE `tree` (
     `parent_id` bigint(20) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_parent_id` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;gi
 
 
 INSERT INTO `tree` (`id`, `name`, created_at, created_by, parent_id)
@@ -14,6 +14,6 @@ INSERT INTO `tree` (`id`, `name`, created_at, created_by, parent_id)
     FROM `hosts`
     WHERE dir = true;
 
-ALTER TABLE `hosts` DROP CONSTRAINT `fk_host_host`
+ALTER TABLE `hosts` DROP CONSTRAINT `fk_host_host`;
 DELETE FROM `hosts` WHERE dir = true;
-ALTER TABLE `hosts` DROP COLUMN `dir`
+ALTER TABLE `hosts` DROP COLUMN `dir`;
