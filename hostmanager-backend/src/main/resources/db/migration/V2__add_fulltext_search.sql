9c08b03db91d6e52a9922722c257927b5dc00bca
Yegor H <procezorjk5@gmail.com>
2023-11-26 23:08:31 +0500

    move to postgresql

--- hostmanager-backend/src/main/resources/db/migration/V2__add_fulltext_search.sql
+++
@@ -1,2 +0,0 @@
-CREATE FULLTEXT INDEX idx_text_hosts_name_address ON hosts (name, address);
-CREATE FULLTEXT INDEX idx_text_notes_title_body ON notes (title, text);
--- hostmanager-backend/src/main/resources/db/migration/V3__separate_tree.sql
+++ hostmanager-backend/src/main/resources/db/migration/V3__separate_tree.sql
@@ -1,19 +1,16 @@
-CREATE TABLE `tree` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `created_at` datetime(6) NOT NULL,
-    `created_by` bigint(20) DEFAULT NULL,
-    `parent_id` bigint(20) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `idx_parent_id` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;gi
+CREATE TABLE tree (
+    id bigserial PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at timestamp NOT NULL,
+    created_by bigint DEFAULT NULL,
+    parent_id bigint DEFAULT NULL
+);
 
-
-INSERT INTO `tree` (`id`, `name`, created_at, created_by, parent_id)
-    SELECT `id`, `name`, IFNULL(created_at, now()), created_by, parent_id
-    FROM `hosts`
+INSERT INTO tree (id, name, created_at, created_by, parent_id)
+    SELECT id, name, COALESCE(created_at, now()), created_by, parent_id
+    FROM hosts
     WHERE dir = true;
 
-ALTER TABLE `hosts` DROP CONSTRAINT `fk_host_host`;
-DELETE FROM `hosts` WHERE dir = true;
-ALTER TABLE `hosts` DROP COLUMN `dir`;
+ALTER TABLE hosts DROP CONSTRAINT fk_host_host;
+DELETE FROM hosts WHERE dir = true;
+ALTER TABLE hosts DROP COLUMN dir;
